ad8482815c7310a2a4282f84f1a1cc541eb92c8f
sarthakporwal <143891620+sarthakporwal@users.noreply.github.com>
2025-08-16 14:59:54 +0530

    Time: 0 ms (100%), Space: 23.6 MB (21.44%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -1,27 +1,37 @@
 class Solution {
 public:
-    ListNode* curr;
-    void solve(ListNode* head){
-        if(head == NULL) return;
-
-        solve(head->next);
-
-        ListNode* temp = curr->next;
-        if(curr->next == NULL){
-            return;
-        }else if(head == curr){
-            head->next = NULL;
-            return;
+    ListNode* reverse(ListNode* head){
+        if(head == NULL || head->next == NULL){
+            return head;
         }
-        curr->next = head;
-        head->next = (temp == head) ? NULL : temp;
-        curr = temp;
+
+        ListNode* last = reverse(head->next);
+        head->next->next = head;
+        head->next = NULL;
+        return last;
     }
 
     void reorderList(ListNode* head) {
-        curr = head;
+        
+        ListNode* slow = head;
+        ListNode* fast = head;
 
-        solve(head);
+        while(fast && fast->next){
 
+            slow = slow->next;
+            fast = fast->next->next;
+
+        }
+
+        ListNode* rev = reverse(slow);
+        ListNode* curr = head;
+        while(rev->next != NULL){
+            ListNode* temp = curr->next;
+            curr->next = rev;
+            ListNode* tempr = rev->next;
+            rev->next = temp;
+            curr = temp;
+            rev = tempr;
+        }
     }
 };
